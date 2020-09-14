3f4043836aa0e53c59c18efbd8ea1ac6e1dbc445
MomenKoprossy <momen_koprossy@yahoo.com>
2020-09-14 19:11:04 +0200

    first commit

---
+++ constants/images/woman.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: 'Dirty Deeds Done Dirty Cheap',
+    // image: 'https://source.unsplash.com/mEZ3PoFGs_k/840x840',
+    image: 'https://vignette.wikia.nocookie.net/jjba/images/4/4d/D4c_sbr69.png/revision/latest?cb=20160323142852',
+    price: 180,
+    horizontal: true,
+  },
+  {
+    title: 'Burger',
+    // image: 'https://source.unsplash.com/8Zv6hOS_STE/840x840',
+    image: 'https://www.thebuffaloburger.com/images/storry.png',
+    price: 220,
+  },
+  {
+    title: "Stand out from the crowd with the...",
+    // image: 'https://source.unsplash.com/BKxipJKe3G0/840x840',
+    image: 'https://images.unsplash.com/photo-1503066375319-00fef06b159e?crop=entropy&w=840&h=840&fit=crop',
+    price: 40,
+  },
+  {
+    title: "Women's sport sometimes seems to...",
+    // image: 'https://source.unsplash.com/kFCdfLbu6zA/840x840',
+    image: 'https://images.unsplash.com/photo-1445384763658-0400939829cd?crop=entropy&w=840&h=840&fit=crop',
+    price: 188,
+    horizontal: true,
+  },
+  {
+    title: 'The Glamour Awards is an annual...',
+    // image: 'https://source.unsplash.com/ZOT2Mewzmh8/840x840',
+    image: 'https://images.unsplash.com/photo-1499313843378-eebdb187f629?crop=entropy&w=840&h=840&fit=crop',
+    price: 180,
+  },
+];
